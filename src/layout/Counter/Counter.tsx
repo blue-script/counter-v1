443b938237e3a24c640f5a7e3f9e470e93cdffac
demianov-an <Alexm92@inbox.ru>
2024-01-21 18:27:36 +0600

    fix

--- src/layout/Counter/Counter.tsx
+++ src/layout/Counter/Counter.tsx
@@ -1,17 +1,16 @@
 import {Dispatch} from 'redux'
-import styled, {css} from 'styled-components';
 import Button from '../../components/Button/Button';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from '../../state/store';
 import {RangeValuesType} from '../../state/range-values-reducer';
 import {changeValueAC} from '../../state/value-reducer';
 import {ValueStatesType} from '../../state/value-states-reducer';
-import { S } from './Counter_Styles';
+import {S} from './Counter_Styles';
 
 export const Counter: React.FC = () => {
-  const value = useSelector<AppRootStateType, number>(state=>state.value)
-  const rangeValues = useSelector<AppRootStateType, RangeValuesType>(state=>state.rangeValues)
-  const valueStates = useSelector<AppRootStateType, ValueStatesType>(state=> state.valueStates)
+  const value = useSelector<AppRootStateType, number>(state => state.value)
+  const rangeValues = useSelector<AppRootStateType, RangeValuesType>(state => state.rangeValues)
+  const valueStates = useSelector<AppRootStateType, ValueStatesType>(state => state.valueStates)
   const dispatch = useDispatch<Dispatch>()
   const increaseValueHandler = () => {
     dispatch(changeValueAC(value + 1))
@@ -22,20 +21,22 @@ export const Counter: React.FC = () => {
 
   return (
     <S.Counter>
-      <S.DisplayScore $value={value} $maxvalue={rangeValues.maxValue} $iscorrectvalues={valueStates.isCorrectValues.toString()} $issetvalue={valueStates.isSetValues.toString()}>
+      <S.DisplayScore $value={value} $maxvalue={rangeValues.maxValue}
+                      $iscorrectvalues={valueStates.isCorrectValues.toString()}
+                      $issetvalue={valueStates.isSetValues.toString()}>
         {
           valueStates.isSetValues
-          ? value
-          : valueStates.isCorrectValues
-            ? 'enter values and press "set"'
-            : 'Incorrect value!'
+            ? value
+            : valueStates.isCorrectValues
+              ? 'enter values and press "set"'
+              : 'Incorrect value!'
         }
       </S.DisplayScore>
       <S.EditScore>
-        <Button disabled={disabledInc || !valueStates.isSetValues} title="inc" func={increaseValueHandler}/>
+        <Button disabled={disabledInc || !valueStates.isSetValues} title='inc' func={increaseValueHandler}/>
         <Button
           disabled={disabledReset || !valueStates.isSetValues}
-          title="reset"
+          title='reset'
           func={resetScoreHandler}
         />
       </S.EditScore>
